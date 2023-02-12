b660fbfbbb902cdd894a516db41ed36bdaa2a152
Bohdan <Bohdan.Zots@quest.com>
2023-02-12 16:37:40 +0100

    Updated token..again

--- src/store/reducers/repositories/repositoriesAction.ts
+++ src/store/reducers/repositories/repositoriesAction.ts
@@ -25,7 +25,7 @@ export const fetchRepositories = createAsyncThunk<IResponseSchema, any, any>(
         `${API_URL}/search/repositories?q=${searchParam}&per_page=${perPage}&page=${currentPage}`,
         {
           headers: {
-            Authorization: "token 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",
+            Authorization: "token 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",
           },
         },
       );
